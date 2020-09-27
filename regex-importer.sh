cb2893522ef67b4e2d06ce4e3fd460bc943c4c4e
Radosław Kierznowski <radek.kierznowski@outlook.com>
2020-09-27 14:27:57 +0200

    Feat: Run with param

--- regex-importer.sh
+++ regex-importer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-input="regex.txt"
-while IFS= read -r line
-do
-	pihole --regex "$line"
-done < "$input"
+input=$1
+if [ -z "$input" ]; then
+	echo "Podaj nazwe pliku z wyrazeniami RegEx jako parametr skryptu"
+	exit
+else
+	continue
+fi
+
+while read -r line; do
+	if [ "$line" = "" || "$line" = "#"* ]; then
+		continue
+	else
+		#pihole --regex "$line"
+		echo "Dodano $line"
+	fi
+done <"$input"
